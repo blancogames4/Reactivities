42d61a64b2fc2a7fac23805d0fb90e32ea913bb3
Gerardo Blanco <blancogames4@outlook.com>
2022-05-23 21:56:25 -0400

    setion

--- API/Startup.cs
+++ API/Startup.cs
@@ -13,6 +13,10 @@ using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
 using Persistence;
 using Microsoft.EntityFrameworkCore;
+using MediatR;
+using Application.Activities;
+using Application.Core;
+using API.Extensions;
 
 namespace API
 {
@@ -30,22 +34,7 @@ namespace API
         {
 
             services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(_config.GetConnectionString ("DefaultConnection"));
-
-            });
-            services.AddCors(opt=>
-            {
-                opt.AddPolicy("CorsPolicy", policy=>
-                {
-                    policy.AllowAnyMethod().AllowAnyHeader().AllowAnyOrigin ();
-                });
-            });
+            services.AddApplicationServices(_config);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -63,6 +52,7 @@ namespace API
             app.UseRouting();
 
             app.UseCors("CorsPolicy");
+
 
             app.UseAuthorization();
 
